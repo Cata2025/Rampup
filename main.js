081a2017969025fec07e082cfa78884abcd48b27
Cata2025 <cblvallada@gmail.com>
2024-04-29 18:42:40 +0200

    improve main.js

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-function fibonacci(name){
-    console.log("Estoy pensando" + name) 
+
+function Fibonacci (number1,number2,number3){
+    let arrFibonacci = [number1,number2];
+    let lastNumber = number2;
+    let secondToLastNumber = number1;
+    while (lasNumber + secondToLastNumber<number3)
+
+    {
+        arrFibonacci.push(lastNumber + secondToLastNumber);
+        secondToLastNumber = lastNumber;
+        lastNumber = lastNumber + secondToLastNumber;
+    }
+    return arrFibonacci;
 }
-fibonacci(" Emiliano")
+console.log(Fibonacci(5,3,200));
+
